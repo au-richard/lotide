cd40e387d06f4aff8fe5f759495d87e94bc1520e
Richard <au.richard4@gmail.com>
2022-02-08 22:56:24 -0800

    Without (Not Finished)

---
+++ without.js
@@ -0,0 +1,32 @@
+const eqArrays = (arr1, arr2) => {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i] || arr1.length !== arr2.length) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = (arr1, arr2) => {
+  if (eqArrays()) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  }
+  console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+};
+
+const without = (source, remove) => {
+  let filtered = [];
+  for (let i = 0; i < source.length; i++) {
+    if (source[i] !== remove[i]) {
+      filtered.push(source[i]);
+    }
+  }
+  return filtered;
+};
+
+without([1, 2, 3], [1]) // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]);
+//assertArraysEqual(words, ["hello", "world", "lighthouse"]);
