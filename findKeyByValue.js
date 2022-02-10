84a57067e5f0dfb045e9c985e9d41e513de58842
Richard <au.richard4@gmail.com>
2022-02-09 20:20:01 -0800

    Finding Object By Input Value Function

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = (tvShow, genre) => {
+  let result = "";
+  for (const key of Object.keys(tvShow)) {
+    if (tvShow[key] === genre) {
+      result = key;
+    } else {
+      result = undefined;
+    }
+  }
+  return result;
+}
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
