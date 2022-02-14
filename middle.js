c201566b3902da0b974b63430aecf160891fbcc9
Richard <au.richard4@gmail.com>
2022-02-14 15:15:31 -0800

    Modified middle.js to use modules, added middleTest.js

--- middle.js
+++ middle.js
@@ -1,26 +1,3 @@
-const eqArrays = (arr1, arr2) => {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i] || arr1.length !== arr2.length) {
-      return false;
-    }
-  }
-  return true;
-};
-
-//let test = eqArrays([1, 2], [1, 2]);
-//console.log(test);
-
-
-const assertArraysEqual = (arr1, arr2) => {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
-
-
 const middle = (arr) => {
   let output = [];
   let center = (Math.floor(arr.length / 2));
@@ -35,8 +12,5 @@ const middle = (arr) => {
   return output;
 };
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-assertArraysEqual(middle([1, 2]), []); // => []
-assertArraysEqual(middle([7, 4, 1, 9, 8, 6]), [1, 9]); // => [1, 9]
+module.exports = middle;
+
