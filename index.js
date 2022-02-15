2496ffad91542555c90dc24f16c16293659c5e6e
Richard <au.richard4@gmail.com>
2022-02-14 17:02:10 -0800

    Created index.js for exporting lotide files

---
+++ index.js
@@ -0,0 +1,49 @@
+// index.js
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./const assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const head = require('./head');
+const letterPosition = require('./letterPosition');
+const map = require('./map');
+const middle = require('./middle');
+const tail = require('./tail');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+
+
+
+module.exports = {
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  findKeyByValue: findKeyByValue,
+  findKey: findKey,
+  head: head,
+  letterPosition: letterPosition,
+  map: map,
+  middle: middle,
+  tail: tail,
+  takeUntil: takeUntil,
+  without: without
+};
+
+assertObjectsEqual;
+countLetters;
+countOnly;
+eqArrays;
+eqObjects;
+letterPosition;
+map.
+  takeUntil.js;
+without.js;
